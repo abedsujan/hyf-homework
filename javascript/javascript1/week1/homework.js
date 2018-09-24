79d7841d7582d31da8a332cb5bc5660bb4c8a9b4
Johanna Baguio <johbaguio@gmail.com>
2018-09-24 17:15:46 +0200

    Added js codes for HOUSEY-PRICEY task on JS homework

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -25,3 +25,25 @@ else {
 
 console.log('Your dog will be ' + dogYear +' ' + ageType + ' years old in ' + dogYearFuture + '.');
 
+
+// HOUSEY-PRICEY
+
+let houseWidthInMeters = 8;
+let houseLengthInMeters = 10;
+let houseDepthInMeters = 10; 
+let gardenSizeInM2 = 100;
+let volumeInMeters = houseWidthInMeters * houseLengthInMeters * houseDepthInMeters;
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let houseCost = 2500000;
+let payingStatus;
+
+if (housePrice < houseCost){
+    payingStatus ='too much';
+}
+else{
+    payingStatus = 'too little';
+}
+
+console.log(housePrice);
+console.log('You are paying ' + payingStatus + ' for the house.');
+
