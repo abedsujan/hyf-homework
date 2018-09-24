fcdef4e63db346218b3dadf3ace2b8b90e862a69
Johanna Baguio <johbaguio@gmail.com>
2018-09-24 17:07:19 +0200

    Added js codes for GOODBOY-OLDBOY task in JS homework

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -5,3 +5,23 @@ let yearFuture = 2030;
 let age = yearFuture - yearOfBirth;
 
 console.log('You will be ' + age + ' years old in ' + yearFuture +'.');
+
+
+// GOODBOY - OLDBOY
+
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true;
+let ageType;
+
+if (shouldShowResultInDogYears){
+    dogYear *= 7;
+    ageType = 'dog';
+} 
+else {
+    ageType = 'human';
+}
+
+console.log('Your dog will be ' + dogYear +' ' + ageType + ' years old in ' + dogYearFuture + '.');
+
